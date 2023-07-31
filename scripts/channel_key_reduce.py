aabfc2eafbf44f665ab64a2b944a208b2a14a0b7
Dmytro Holub <heap3d@gmail.com>
2023-07-31 15:30:16 +0300

    added: channel_to_setup, key_reduce, hierarch_fix

---
+++ scripts/channel_key_reduce.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# ================================
+# (C)2023 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+# EMAG
+# Reduce Keys
+# Reduces the number of keys on selected channels
+
+import lx
+
+
+def main():
+    lx.eval('channel.keyReduce')
+
+
+if __name__ == '__main__':
+    main()
