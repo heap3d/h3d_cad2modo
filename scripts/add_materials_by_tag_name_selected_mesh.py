ceba8342e19e583134c910b3c40f7343ae2413ae
Dmytro Holub <heap3d@gmail.com>
2024-02-17 20:55:11 +0200

    added: add materials by tag name for selected mesh items

--- scripts/add_materials_by_tag_name_selected_mesh.py
+++ scripts/add_materials_by_tag_name_selected_mesh.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
 # ================================
-# (C)2022-2024 Dmytro Holub
+# (C)2024 Dmytro Holub
 # heap3d@gmail.com
 # --------------------------------
 # modo python
 # EMAG
-# add material by polygon tag to all mesh items
+# add material by polygon tag to selected mesh items
 
 import lx
 import modo
@@ -15,15 +15,15 @@ from h3d_cad2modo.scripts.unassigned_ptags import assign_materials_to_unassigned
 
 
 def main():
-    print('add_materials_by_tag_name.py start...')
+    print('add_materials_by_tag_name_selected_mesh.py start...')
 
-    meshes = modo.Scene().meshes
+    meshes = modo.Scene().selectedByType(itype=c.MESH_TYPE)
     masks = modo.Scene().items(itype=c.MASK_TYPE)
     assign_materials_to_unassigned_ptags(meshes, masks)
 
     lx.eval('select.type item')
 
-    print('add_materials_by_tag_name.py done.')
+    print('add_materials_by_tag_name_selected_mesh.py done.')
 
 
 if __name__ == '__main__':
