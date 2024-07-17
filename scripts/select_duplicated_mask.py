580b508bce082a165951a735637754f33720c588
Dmytro Holub <heap3d@gmail.com>
2024-07-17 19:02:13 +0300

    updated: empty tag return

--- scripts/select_duplicated_mask.py
+++ scripts/select_duplicated_mask.py
@@ -11,8 +11,9 @@ import modo
 import lx
 
 from h3d_utilites.scripts.h3d_debug import H3dDebug
-from h3d_utilites.scripts.h3d_utils import replace_file_ext
 
+
+EMPTY_PTAG = 'Material'
 
 COMPARE_BY_MATERIAL_TAG = False
 COMPARE_BY_SELECTION_SET_TAG = False
@@ -36,7 +37,7 @@ USERVAL_COMPAREBY_LIST = f'{CBL_ALL};{CBL_ITEM_MASK};{CBL_MATERIAL};{CBL_SELECTI
 # clear_selection user.value
 USERVAL_CLEAR_SELECTION_NAME = 'h3d_select_duplicated_clear_selection'
 USERVAL_CLEAR_SELECTION_USERNAME = 'clear selection'
-userVal_clear_selection_dialogname = 'turn ON to clear selection'
+USERVAL_CLEAR_SELECTION_DIALOGNAME = 'turn ON to clear selection'
 
 # delete_material_mask user.value
 USERVAL_DELETE_MATERIAL_MASK_NAME = 'H3D_SELECT_DUPLICATED_DELETE_MATERIAL_MASK'
@@ -61,11 +62,14 @@ def get_mask_list():
 
 def get_ptag(mask_item):
     ptag = mask_item.channel('ptag').get()
+    if not ptag:
+        return EMPTY_PTAG
     return ptag
 
 
 def get_ptag_type(mask_item):
     ptyp = mask_item.channel('ptyp').get()
+    printd(f'{mask_item.name} ptag type: <{ptyp}>', 4)
     return ptyp
 
 
@@ -76,16 +80,30 @@ def get_item_mask(mask_item):
 
 
 def are_mask_equal(mask1, mask2):
+    printd(f'comparing <{mask1.name}> vs <{mask2.name}>...', 2)
     childrens1 = len(mask1.children(recursive=False, itemType='mask'))
     if childrens1 != 0:
+        printd('childrens1 != 0: Not Equal', 3)
         return False
     childrens2 = len(mask2.children(recursive=False, itemType='mask'))
     if childrens2 != 0:
+        printd('childrens2 != 0: Not Equal', 3)
         return False
     if get_item_mask(mask1) == get_item_mask(mask2):
+        printd('identical item mask', 3)
         if get_ptag_type(mask1) == get_ptag_type(mask2):
+            printd('identical ptag type', 3)
             if get_ptag(mask1) == get_ptag(mask2):
+                printd('identical ptag', 3)
+                printd('Equal', 3)
                 return True
+            else:
+                printd('non-identical ptag', 3)
+        else:
+            printd('non-identical ptag type', 3)
+    else:
+        printd('non-identical item mask', 3)
+    printd('Not Equal', 3)
     return False
 
 
@@ -100,14 +118,21 @@ def main():
         # processed_tag_list = {}
         scene_mask_list = get_mask_list()
         tmp_scene_list = set(scene_mask_list)
+        printi(scene_mask_list, 'scene_mask_list:')
+        printi(tmp_scene_list, 'tmp_scene_list:')
+        printd('scene_mask_list loop:')
         for mask in scene_mask_list:
+            h3dd.print_debug(f'mask: {mask.name}', 1)
             if mask in tmp_scene_list:
                 tmp_scene_list.remove(mask)
+                printd(f'<{mask.name}> removed from tmp_scene_list', 1)
             processed_list = set(tmp_scene_list)
+            printd('processed_list loop:', 1)
             for compared_mask in processed_list:
                 if are_mask_equal(mask, compared_mask):
                     tmp_scene_list.remove(compared_mask)
                     duplicated_mask_list.add(compared_mask)
+                    printd(f'dublicated mask detected: <{compared_mask.name}>, 2')
         # clear selection
         scene.deselect()
         # select duplicated masks
@@ -125,4 +150,7 @@ def main():
 if __name__ == '__main__':
     scene = modo.Scene()
     h3dd = H3dDebug(enable=True, file=scene.name + '.log')
+    printd = h3dd.print_debug
+    printi = h3dd.print_items
+
     main()
