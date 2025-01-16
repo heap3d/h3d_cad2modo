c3a44e4d0624daefa4ed215f5c76f8e19a1c111d
Dmytro Holub <heap3d@gmail.com>
2025-01-16 13:40:46 +0200

    fix non-actual chanMods links initial submit

---
+++ scripts/fix_chanmods.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# ================================
+# (C)2025 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+# EMAG
+# fix chanMods graph
+# update chanMods graph for selected items to display connection in the items tree
+# ================================
+
+import modo
+
+
+def main():
+    selected = modo.Scene().selected
+
+
+if __name__ == '__main__':
+    main()
